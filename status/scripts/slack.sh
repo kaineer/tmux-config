401016c46c7c66facfffd61da42eecfe9efee51e
Sergey Kluchkovsky <kaineer@gmail.com>
2020-04-22 13:38:10 +0500

    Removes obsolete code

--- status/scripts/slack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$SSH_CLIENT" == "" ]; then
-  if [ "1" == "$(ps aux | grep slack | grep zygote | wc -l)" ]; then
-    echo '[#[bold,fg=green,bg=black]slack#[default]]';
-  else
-    echo '[#[bold,fg=red,bg=black]slack#[default]]';
-  fi;
-fi
